641b70825b3ee788a33c342a32f57f72303f63dc
Maurício Linhares <mauricio.linhares@gmail.com>
2011-01-01 15:58:44 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'mysql2', '0.2.6'
+gem 'unicorn', '3.2.1'
+gem 'sunspot', '1.2.1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotTutorial::Application.config.secret_token = '6f2b4bccf29e76db6c6ec7175f3ef8f1777acd135115023f6fbaf05c38af93585e6df46e637166dc72d95dfd730e166b4b9e787a0c7c240aaa9a8074e9c8df43'
